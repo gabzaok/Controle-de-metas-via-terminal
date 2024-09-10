1a8ba76d7cc86385b1d036261282d6e68c790b59
Gabrielle Carvalho <gaabmoralles@gmail.com>
2024-09-10 12:13:41 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
 
-const start = async() => {
-  
+let meta = {
+    value:"Tomar 3L de água por dia",
+    checked: false,
+}
+
+let metas = [meta]
+
+
+const cadastrarMeta = async () => { 
+    const meta = await input({message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+    metas.push({
+        value: meta, checked: false
+    })
+}
+
+
+const start = async () => {
+
     while (true) {
 
-       const opcao = await select({
-        message:"Menu >",
-        choices: [
-            {
-                name: "Cadastrar meta",
-                value:"cadastrar"
-            },
-            {
-                name:"Listar Metas",
-                value:"listar"
-            },
-            {
-                name: "Sair",
-                value: "sair"
-            }
-        ]
-       })
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Metas",
+
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
 
         switch (opcao) {
             case "cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
+
             case "listar":
                 console.log("vamos listar")
                 break
