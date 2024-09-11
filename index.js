d43fa182d4b9b61f131e39b47b6964175e44af36
Gabrielle Carvalho <gaabmoralles@gmail.com>
2024-09-11 17:25:03 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -62,8 +62,23 @@ const metasRealizadas = async () => {
         return
     }
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked  
+    })
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas :D")
+        return
+    }
+
+    await select({
+        message:"Metas Abertas" + abertas.length,
+        choices:[...abertas]
     })
 }
 
@@ -89,6 +104,10 @@ const start = async () => {
 
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -107,6 +126,10 @@ const start = async () => {
 
             case "realizadas":
                 await metasRealizadas()
+                break
+
+            case "abertas":
+                await metasAbertas()
                 break
 
             case "sair":
